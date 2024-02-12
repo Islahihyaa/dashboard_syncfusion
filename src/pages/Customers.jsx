bbe6bd8fee79042c02e0881ef3d65faa117dcf68
Islahihyaa <islah.ihya@gmail.com>
2024-02-12 22:18:47 +0700

    Create Employees page

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,8 +1,28 @@
+import { ColumnDirective, GridComponent, ColumnsDirective, Page, Toolbar, Selection, Edit, Sort, Filter, Inject } from '@syncfusion/ej2-react-grids'
+import { Header } from '../components'
 import React from 'react'
+import { customersData, customersGrid } from '../data/dummy'
 
 const Customers = () => {
   return (
-    <div>Customers</div>
+    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-xl'>
+    <Header category="Page" title="Customers" />
+    <GridComponent 
+      id='gridcomp'
+      dataSource={customersData}
+      allowPaging
+      allowSorting
+      toolbar={['Delete']}
+      width='auto'
+      editSettings={{ allowDeleting: true, allowEditing: true}}
+    >
+      <ColumnsDirective>
+        {customersGrid.map((item, index) => ( <ColumnDirective key={index} {...item} />
+        ))}
+      </ColumnsDirective>
+      <Inject services={[Page, Toolbar, Selection, Edit, Sort, Filter]} />
+    </GridComponent>
+  </div>
   )
 }
 
